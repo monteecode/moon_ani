ddffa090c8545356586f69d3e56ded48dfb1b3d4
Montensia Banks <montensia@gmail.com>
2021-12-21 15:02:23 -0600

    Added moon images and html to include animation libraries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import newMoon from "./images/new_moon.jpeg";
+import waxing from "./images/waxing_crescent.jpeg";
+import firstQuarter from "./images/first_quarter.jpeg";
+import waxingGibb from "./images/waxing_gibb.jpeg";
+import fullMoon from "./images/full_moon.jpeg";
+import waningGib from "./images/waning_gib.jpeg";
+import lastQuarter from "./images/last_quarter.jpeg";
+import waning from "./images/waning_cres.jpeg";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Moon Phase Animation</h1>
       </header>
+      <div className="new moons">
+        <h2>New Moon</h2>
+        <img src={newMoon} alt="new moon" />
+      </div>
+      <div className="waxing moons">
+        <h2>Waxing Cresent Moon</h2>
+        <img src={waxing} alt="waxing cresent moon" />
+      </div>
+      <div className="first moons">
+        <h2>First Quarter Moon</h2>
+        <img src={firstQuarter} alt="first quarter moon" />
+      </div>
+      <div className="waxing moons">
+        <h2>Waxing Gibbous Moon</h2>
+        <img src={waxingGibb} alt="waxing gibbous moon" />
+      </div>
+      <div className="full moons">
+        <h2>Full Moon</h2>
+        <img src={fullMoon} alt="full moon" />
+      </div>
+      <div className="waningGib moons">
+        <h2>Waning Gibbous Moon</h2>
+        <img src={waningGib} alt="waning gibbous moon" />
+      </div>
+      <div className="last moons">
+        <h2>Last Quarter Moon</h2>
+        <img src={lastQuarter} alt="last quarter moon" />
+      </div>
+      <div className="waning">
+        <h2>Waning Cresent Moon</h2>
+        <img src={waning} alt="waning cresent moon" />
+      </div>
     </div>
   );
 }
