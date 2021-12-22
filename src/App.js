638bfc0d259b0540916362f6e8a2dc0e37dec379
Montensia Banks <montensia@gmail.com>
2021-12-22 13:56:26 -0600

    Changed code from scrolling horizontally to standard vertical scrolling.  Also, refactor app component to a class component for handling state.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import newMoon from "./images/new_moon.jpeg";
 import waxing from "./images/waxing_crescent.jpeg";
 import firstQuarter from "./images/first_quarter.jpeg";
@@ -9,46 +10,79 @@ import waning from "./images/waning_cres.jpeg";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>Moon Phase Animation</h1>
-      </header>
-      <div className="new moons">
-        <h2>New Moon</h2>
-        <img src={newMoon} alt="new moon" />
-      </div>
-      <div className="waxing moons">
-        <h2>Waxing Cresent Moon</h2>
-        <img src={waxing} alt="waxing cresent moon" />
-      </div>
-      <div className="first moons">
-        <h2>First Quarter Moon</h2>
-        <img src={firstQuarter} alt="first quarter moon" />
-      </div>
-      <div className="waxing moons">
-        <h2>Waxing Gibbous Moon</h2>
-        <img src={waxingGibb} alt="waxing gibbous moon" />
-      </div>
-      <div className="full moons">
-        <h2>Full Moon</h2>
-        <img src={fullMoon} alt="full moon" />
-      </div>
-      <div className="waningGib moons">
-        <h2>Waning Gibbous Moon</h2>
-        <img src={waningGib} alt="waning gibbous moon" />
-      </div>
-      <div className="last moons">
-        <h2>Last Quarter Moon</h2>
-        <img src={lastQuarter} alt="last quarter moon" />
-      </div>
-      <div className="waning">
-        <h2>Waning Cresent Moon</h2>
-        <img src={waning} alt="waning cresent moon" />
-      </div>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentImage: "",
+      currentPositon: 0,
+    };
+  }
 
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Moon Phase Animation</h1>
+        </header>
+        <div className="allMoons">
+          <div className="new moons">
+            <h2>New Moon</h2>
+            <img className="new" src={newMoon} alt="new moon" />
+          </div>
+          <div className="moons">
+            <h2>Waxing Cresent Moon</h2>
+            <img
+              className="waxingCres"
+              src={waxing}
+              alt="waxing cresent moon"
+            />
+          </div>
+          <div className="moons">
+            <h2>First Quarter Moon</h2>
+            <img
+              className="first"
+              src={firstQuarter}
+              alt="first quarter moon"
+            />
+          </div>
+          <div className="moons">
+            <h2>Waxing Gibbous Moon</h2>
+            <img
+              className="waxingGibb"
+              src={waxingGibb}
+              alt="waxing gibbous moon"
+            />
+          </div>
+          <div className="moons">
+            <h2>Full Moon</h2>
+            <img className="full" src={fullMoon} alt="full moon" />
+          </div>
+          <div className="moons">
+            <h2>Waning Gibbous Moon</h2>
+            <img
+              className="waningGib"
+              src={waningGib}
+              alt="waning gibbous moon"
+            />
+          </div>
+          <div className="moons">
+            <h2>Last Quarter Moon</h2>
+            <img className="last" src={lastQuarter} alt="last quarter moon" />
+          </div>
+          <div className="moons">
+            <h2>Waning Cresent Moon</h2>
+            <img
+              className="waningCres"
+              src={waning}
+              alt="waning cresent moon"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
